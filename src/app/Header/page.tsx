d4cd70d3e33aa1ea3cd9ef80b49bc8214f5cd292
TokunagaX <officialxyc@gmail.com>
2023-04-12 00:07:14 +0800

    update Home component

--- src/app/Header/page.tsx
+++ src/app/Header/page.tsx
@@ -1,26 +1,21 @@
 export default function Header() {
     return (
-        <nav className="fixed w-full flex justify-between align-center py-6 pl-12 pr-10 bg-white shadow-md">
-            <h3 className="font-bold text-xl">Stefan.dev</h3>
-            <ul className="flex gap-8 font-bold">
-                <li>Home</li>
-                <li>About</li>
-                <li>Projects</li>
-                <li>Contact</li>
+        <nav className="fixed w-full flex justify-between align-center py-[28px] pl-[40px] pr-[50px] bg-white shadow-[0_0_10px_rgba(0,0,0,0.09)]">
+            <h3 className="font-bold text-3xl">Tokunaga-X</h3>
+            <ul className="flex gap-8">
+                <li className="font-semibold text-[1.7rem] text-[#2d2e32]">
+                    Home
+                </li>
+                <li className="font-semibold text-[1.7rem] text-[#2d2e32]">
+                    About
+                </li>
+                <li className="font-semibold text-[1.7rem] text-[#2d2e32]">
+                    Projects
+                </li>
+                <li className="font-semibold text-[1.7rem] text-[#2d2e32]">
+                    Contact
+                </li>
             </ul>
         </nav>
     )
 }
-// align-items: center;
-//     background-color: #fff;
-//     box-shadow: 0 0 10px rgba(0,0,0,.09);
-//     display: flex;
-//     font-size: 1.7rem;
-//     height: auto;
-//     justify-content: space-between;
-//     left: 0;
-//     padding: 25px 40px 25px 50px;
-//     position: fixed;
-//     top: 0;
-//     width: 100%;
-//     z-index: 200;
--- src/app/Home/page.tsx
+++ src/app/Home/page.tsx
@@ -4,10 +4,10 @@ import MyselfImg from "../../../public/Myself.png"
 
 export default function Home() {
     return (
-        <div className="h-screen flex flex-col items-center bg-grey-bg">
-            <div className="flex justify-center items-center h-3/4 w-5/6">
-                <div>
-                    <h1 className="font-bold text-6xl my-6">
+        <div className="h-screen flex flex-col items-center bg-grey-bg ">
+            <div className="relative flex justify-center items-center gap-[10rem] my-0 mx-auto px-16 h-[65rem] max-w-[107rem]">
+                <div className="max-w-[50rem]">
+                    <h1 className="font-bold text-[5.5rem] my-8 leading-tight">
                         Front-End React Developer
                         <Image
                             src={HelloImg}
@@ -15,16 +15,29 @@ export default function Home() {
                             className="w-16 inline-block"
                         ></Image>
                     </h1>
-                    <p className="font-special text-xl text-gray-600">
+                    <p className="font-special text-[1.8rem] text-[#767676] font-medium">
                         Hi, I&apos;m Xu Yuchao. A passionate Front-end React
-                        Developer based in Hangzhou, China.
+                        Developer based in Hangzhou, China. 📍
                     </p>
                 </div>
                 <Image
                     src={MyselfImg}
                     alt="myself"
-                    className="w-80 animate-morph border-2 border-solid border-black"
+                    className="w-[35rem] animate-morph border-[3px] border-solid border-black"
                 ></Image>
+                <div className="absolute bottom-0 left-0 flex items-center">
+                    <p className="font-special text-lg border-r-2 border-solid border-black pr-8 mr-24">
+                        Tech Stack
+                    </p>
+                    <ul className="flex gap-10 list-none flex-wrap">
+                        <li className="radius-1/2 bg-white w-14 h-14 rounded-full shadow-[0_0_10px_rgba(0,0,0,0.09)]"></li>
+                        <li className="radius-1/2 bg-white w-14 h-14 rounded-full shadow-[0_0_10px_rgba(0,0,0,0.09)]"></li>
+                        <li className="radius-1/2 bg-white w-14 h-14 rounded-full shadow-[0_0_10px_rgba(0,0,0,0.09)]"></li>
+                        <li className="radius-1/2 bg-white w-14 h-14 rounded-full shadow-[0_0_10px_rgba(0,0,0,0.09)]"></li>
+                        <li className="radius-1/2 bg-white w-14 h-14 rounded-full shadow-[0_0_10px_rgba(0,0,0,0.09)]"></li>
+                        <li className="radius-1/2 bg-white w-14 h-14 rounded-full shadow-[0_0_10px_rgba(0,0,0,0.09)]"></li>
+                    </ul>
+                </div>
             </div>
         </div>
     )
