9fda4e4df06da5738fe3b1076cbdf768bc0db335
TokunagaX <officialxyc@gmail.com>
2023-06-08 00:41:45 +0800

    update project part: details

--- src/app/Projects/page.tsx
+++ src/app/Projects/page.tsx
@@ -1,29 +1,72 @@
+import Image, { StaticImageData } from "next/image"
+
+import BlogPic from "../../../public/blog-site.png"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import Image from "next/image"
+import JustLuxury from "../../../public/just-luxury.png"
 import MovingPicture from "../../../public/moving-picture.png"
+import SpeedSearch from "../../../public/speed-search.png"
 import { faClapperboard } from "@fortawesome/free-solid-svg-icons"
 import { faGithub } from "@fortawesome/free-brands-svg-icons"
 
+const CardArray = [
+    {
+        title: "Moving-Picture",
+        img: MovingPicture,
+        des: `A gym website is a comprehensive resource for fitness information, class schedules, membership options, and tools to help users achieve their fitness goals.`,
+        skills: ["React", "Express"],
+        codeLink: "https://github.com/Tokunaga-X/Moving-Picture",
+    },
+    {
+        title: "Just-Luxury",
+        img: JustLuxury,
+        des: `Luxury house website that showcases amazing real estate properties. Great design and code using the Vue tech stack. Built this to learn the Vue framework and improve my front-end skills.`,
+        skills: ["Vue", "Sass"],
+        codeLink: "https://github.com/Tokunaga-X/Just-Luxury",
+    },
+    {
+        title: "My blog",
+        img: BlogPic,
+        des: `My blog website contains learning notes and some random thoughts. In the future, I may add some movie or book reviews since I am a movie lover.`,
+        skills: ["Github-pages", "Jekyll"],
+        codeLink: "",
+    },
+    {
+        title: "SpeedSearch",
+        img: SpeedSearch,
+        des: `This is a Chrome extension that has been designed with the aim of providing a fast and efficient way to search for information on a range of different websites.`,
+        skills: ["ChromeExtension", "TimeSaver"],
+        codeLink: "https://github.com/Tokunaga-X/speedSearch",
+    },
+]
+
 export default function Projects() {
-    const getCard = () => {
+    const getCard = (
+        title: string,
+        coverImg: StaticImageData,
+        description: string,
+        skillArray: string[],
+        codeLink: string,
+        demoLink: string = "/"
+    ) => {
         return (
             <>
-                <div className="w-[53rem] rounded-[1.7rem] shadow-[0_0_10px_rgba(0,0,0,.2)] overflow-hidden">
+                <div className="w-[53rem] rounded-[1.7rem] shadow-[0_0_10px_rgba(0,0,0,.2)] overflow-hidden flex items-center">
                     <a
-                        href="https://github.com/Tokunaga-X"
+                        href={codeLink}
                         target="_blank"
                         rel="noopener noreferrer"
+                        className="w-full"
                     >
                         <Image
-                            src={MovingPicture}
+                            src={coverImg}
                             alt="moving-picture"
-                            className="translate-y-0 transition-transform duration-[10s] hover:translate-y-[-52%]"
+                            className="w-full translate-y-0 transition-transform duration-[10s] hover:translate-y-[-52%] border-y-4"
                         ></Image>
                     </a>
                 </div>
                 <div className="w-[30rem] mx-auto flex flex-col justify-center items-center text-center text-[1.7rem] font-medium">
                     <h3 className="text-[1.7rem] mb-8 font-bold uppercase">
-                        Moving-Picture
+                        {title}
                         <FontAwesomeIcon
                             icon={faClapperboard}
                             className="text-3xl"
@@ -31,16 +74,14 @@ export default function Projects() {
                         />
                     </h3>
                     <p className="text-center mb-4 text-[#767676]">
-                        A gym website is a comprehensive resource for fitness
-                        information, class schedules, membership options, and
-                        tools to help users achieve their fitness goals.
+                        {description}
                     </p>
                     <div className="flex gap-4 mt-4">
                         <p className="text-center shadow-[0_0_10px_rgba(0,0,0,.1)] text-[1.7rem] font-semibold py-4 px-[1.3rem]">
-                            React
+                            {skillArray[0]}
                         </p>
                         <p className="text-center shadow-[0_0_10px_rgba(0,0,0,.1)] text-[1.7rem] font-semibold py-4 px-[1.3rem]">
-                            Scss
+                            {skillArray[1]}
                         </p>
                     </div>
                     <div className="w-full mt-12 flex justify-evenly gap-4">
@@ -48,7 +89,7 @@ export default function Projects() {
                             <p className="text-[1.7rem] font-medium">Code</p>
                             <a
                                 className=""
-                                href="https://github.com/Tokunaga-X"
+                                href={codeLink}
                                 target="_blank"
                                 rel="noopener noreferrer"
                             >
@@ -58,14 +99,14 @@ export default function Projects() {
                                 />
                             </a>
                         </div>
-                        <div className="flex items-center hover:text-font-blue transition-all duration-500 hover:cursor-pointer gap-[5px]">
+                        <div className="flex items-center hover:text-font-blue transition-all duration-500 hover:cursor-not-allowed gap-[5px]">
                             <p className="text-[1.7rem] font-medium">
                                 Live Demo
                             </p>
                             <a
-                                className=""
-                                href="https://github.com/Tokunaga-X"
-                                target="_blank"
+                                className="hover:cursor-not-allowed"
+                                // href={demoLink}
+                                // target="_blank"
                                 rel="noopener noreferrer"
                             >
                                 <FontAwesomeIcon
@@ -89,10 +130,42 @@ export default function Projects() {
                 </h3>
             </div>
             <ul className="flex flex-col items-center gap-20 max-w-[107rem] mx-auto">
-                <li className="project-card-left">{getCard()}</li>
-                <li className="project-card-right">{getCard()}</li>
-                <li className="project-card-left">{getCard()}</li>
-                <li className="project-card-right">{getCard()}</li>
+                <li className="project-card-left">
+                    {getCard(
+                        CardArray[0].title,
+                        CardArray[0].img,
+                        CardArray[0].des,
+                        CardArray[0].skills,
+                        CardArray[0].codeLink
+                    )}
+                </li>
+                <li className="project-card-right">
+                    {getCard(
+                        CardArray[1].title,
+                        CardArray[1].img,
+                        CardArray[1].des,
+                        CardArray[1].skills,
+                        CardArray[1].codeLink
+                    )}
+                </li>
+                <li className="project-card-left">
+                    {getCard(
+                        CardArray[2].title,
+                        CardArray[2].img,
+                        CardArray[2].des,
+                        CardArray[2].skills,
+                        CardArray[2].codeLink
+                    )}
+                </li>
+                <li className="project-card-right">
+                    {getCard(
+                        CardArray[3].title,
+                        CardArray[3].img,
+                        CardArray[3].des,
+                        CardArray[3].skills,
+                        CardArray[3].codeLink
+                    )}
+                </li>
             </ul>
         </section>
     )
